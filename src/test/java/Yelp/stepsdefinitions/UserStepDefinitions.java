2d19e42ca08a65de8d30c7b26563522bb5fe1346
Lucas Biondi <Lucas.Biondi@sony.com>
2023-11-13 11:35:10 -0300

    Update Pom to support browser autodownload

--- src/test/java/Yelp/stepsdefinitions/UserStepDefinitions.java
+++ src/test/java/Yelp/stepsdefinitions/UserStepDefinitions.java
@@ -1,10 +1,7 @@
 package Yelp.stepsdefinitions;
 
 import Yelp.steps.UserYelp;
-import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import io.cucumber.java.en.*;
 import net.serenitybdd.core.Serenity;
 import net.thucydides.core.annotations.Steps;
 
